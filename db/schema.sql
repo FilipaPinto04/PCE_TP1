c590f5fcaf49bac1ec89320c6d79480c195f92ee
FilipaPinto04 <filipamoraispinto2004@gmail.com>
2026-03-30 23:32:22 +0100

    Schema.sql com tudo

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,23 @@
--- schema.sql
-
 CREATE TABLE IF NOT EXISTS patients (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     genero CHAR(1)
 );
 
+CREATE TABLE IF NOT EXISTS medicos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    genero CHAR(1),
+    especialidade VARCHAR(100)
+);
+
 CREATE TABLE IF NOT EXISTS contacto (
     id SERIAL PRIMARY KEY,
     paciente_id INT REFERENCES patients(id) ON DELETE CASCADE,
+    medico_id INT REFERENCES medicos(id) ON DELETE CASCADE,
     nome VARCHAR(255)
 );
 
--- Note que esta já inclui a coluna contacto_id que faltava
 CREATE TABLE IF NOT EXISTS telecom (
     id SERIAL PRIMARY KEY,
     paciente_id INT REFERENCES patients(id) ON DELETE CASCADE,
@@ -25,14 +30,23 @@ CREATE TABLE IF NOT EXISTS endereco (
     id SERIAL PRIMARY KEY,
     contacto_id INT REFERENCES contacto(id) ON DELETE CASCADE,
     tipo VARCHAR(50),
-    valor TEXT
+    valor VARCHAR (255)
+);
+
+CREATE TABLE IF NOT EXISTS consultas (
+    id SERIAL PRIMARY KEY,
+    paciente_id INT REFERENCES patients(id) ON DELETE CASCADE,
+    medico_id INT REFERENCES medicos(id) ON DELETE SET NULL,
+    data_consulta TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    tipo_consulta VARCHAR(100) 
 );
 
 CREATE TABLE IF NOT EXISTS observacoes (
     id SERIAL PRIMARY KEY,
     paciente_id INT REFERENCES patients(id) ON DELETE CASCADE,
+    consulta_id INT REFERENCES consultas(id) ON DELETE CASCADE,
     estado VARCHAR(50),
-    refer Varchar(70),
+    refer VARCHAR(70),
     dataExecucao TIMESTAMP WITH TIME ZONE
 );
 
@@ -57,4 +71,10 @@ CREATE TABLE IF NOT EXISTS medicao (
     unidade VARCHAR(50),
     sistema VARCHAR(150),
     cod VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS historico (
+    id SERIAL PRIMARY KEY,
+    paciente_id INT REFERENCES patients(id) ON DELETE CASCADE,
+    consulta_id INT REFERENCES consultas(id) ON DELETE SET NULL
 );
